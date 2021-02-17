be409bda55adce67587b94a029c31e4860dda9a4
Silvio Cifone <46993299+silvioCifone@users.noreply.github.com>
2021-02-17 14:46:04 +0100

    Add products_overview_screen.dart
    It's a screen with every items stored in the loadedProducts array. Items are organized in a grid list

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_test/screens/products_overview_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,37 +13,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'My Shop!'),
+      home: ProductsOverviewScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Let's build a shop!",
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
