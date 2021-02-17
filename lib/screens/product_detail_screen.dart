d16b714964d2c29744b19374f4c039efcd1fb682
Silvio Cifone <46993299+silvioCifone@users.noreply.github.com>
2021-02-17 17:10:55 +0100

    Add data fetch
    With the Provider, we can get data for the product we decided to tap in the grid view

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_test/providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   /*final String _title;
@@ -11,9 +13,11 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
+    final selectedProduct =
+        Provider.of<Products>(context).findProductById(productId);
     return Scaffold(
       appBar: AppBar(
-        title: Text("this._title"),
+        title: Text(selectedProduct.title),
       ),
     );
   }
